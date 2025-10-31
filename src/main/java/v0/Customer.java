80dfd56baaa7a0636a5a57f7cb4d5e5eebfa2da1
Gonçalo Lopes <gbsls@iscte-iul.pt>
2025-10-31 13:44:33 +0000

    V0 (#3)
    * Removed Long Method
    
    * Removed long method
    
    * Rerrol wrong code change

--- src/main/java/v0/Customer.java
+++ src/main/java/v0/Customer.java
@@ -32,30 +32,10 @@ public class Customer
 		
 		for (Rental each: _rentals)
 		{
-			double thisAmount = 0;
+			double thisAmount = each.getAmount();
 
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode())
-			{
-				case REGULAR:
-					thisAmount += 2;
-					if (each.getDaysRented() > 2)
-						thisAmount += (each.getDaysRented() - 2) * 1.5;
-					break;
-				case NEW_RELEASE:
-					thisAmount += each.getDaysRented() * 3;
-					break;
-				case CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDaysRented() > 3)
-						thisAmount += (each.getDaysRented() - 3) * 1.5;
-					break;
-			}
-
-			// add frequent renter points
 			frequentRenterPoints++;
 
-			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
 				frequentRenterPoints++;
 
